8e8498f17297b840fb1860f0f6dcfb30a8088860
Bishop <lake.d.bishop@gmail.com>
2022-09-18 01:58:09 -0500

    Added The Styleing from example repo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './componets/Header';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="wrapper">
+      <Header title='React Task Tracker'/>
     </div>
   );
 }
